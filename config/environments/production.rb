58cd473de12ce687ecdcff5066d78cfde01afe68
dina <dina@byteis.com>
2016-09-24 23:59:03 +0200

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,11 @@ Rails.application.configure do
 
 
   #mailer
-  # Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default :charset => "utf-8"
+  Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   # config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
   # Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
@@ -92,15 +92,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default :charset => "utf-8"
   #
-  # config.action_mailer.smtp_settings = {
-  #     address: 'smtp.gmail.com',
-  #     port: 587,
-  #     domain: 'gmail.com',
-  #     authentication: "plain",
-  #     enable_starttls_auto: true,
-  #     user_name: 'dinammorad1@gmail.com',
-  #     password: '123456dodo'
-  # }
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: 'dinammorad1@gmail.com',
+      password: '123456dodo'
+  }
 
 
 
