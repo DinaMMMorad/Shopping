ac952b6113798a9c6caabee637320bdcef9a5066
dina <dina@byteis.com>
2016-09-25 00:58:05 +0200

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
   # }
 
 
-  config.action_mailer.default_url_options = { :host => 'mallonline.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -112,7 +112,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: 'mallonline.herokuapp.com',
+      domain: 'https://mallonline.herokuapp.com',
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: 'dinammorad1@gmail.com',
