bbfe4f8cd1f84d13ff6b77ab7150e39a60bdc7a0
dina <dina@byteis.com>
2016-09-25 00:50:51 +0200

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,23 +85,42 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
+  # config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
+  # Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
+  # #
+  # config.action_mailer.smtp_settings = {
+  #     address: 'smtp.gmail.com',
+  #     port: 587,
+  #     domain: 'gmail.com',
+  #     authentication: "plain",
+  #     enable_starttls_auto: true,
+  #     user_name: 'dinammorad1@gmail.com',
+  #     password: '123456dodo',
+  #     openssl_verify_mode: 'none'
+  # }
+
+
+  onfig.action_mailer.default_url_options = { :host => 'mallonline.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  #
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
+      address: "smtp.gmail.com",
       port: 587,
-      domain: 'gmail.com',
+      domain: 'mallonline.herokuapp.com',
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: 'dinammorad1@gmail.com',
       password: '123456dodo',
       openssl_verify_mode: 'none'
   }
+
+
 
 
 
