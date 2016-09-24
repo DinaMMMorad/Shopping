f2b5a5234b96afda9f677293a14025084eb66605
dina <dina@byteis.com>
2016-09-24 18:31:06 +0200

    change product description type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920235036) do
+ActiveRecord::Schema.define(version: 20160924162859) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -91,17 +91,17 @@ ActiveRecord::Schema.define(version: 20160920235036) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title",                limit: 255
-    t.string   "description",          limit: 255
+    t.text     "description",          limit: 65535
     t.string   "image",                limit: 255
     t.boolean  "is_new"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "picture_file_name",    limit: 255
     t.string   "picture_content_type", limit: 255
     t.integer  "picture_file_size",    limit: 4
     t.datetime "picture_updated_at"
     t.integer  "category_id",          limit: 4
-    t.decimal  "price",                            precision: 10
+    t.decimal  "price",                              precision: 10
     t.float    "width",                limit: 24
     t.float    "height",               limit: 24
   end
