ba1306dd1f1094a1db71b6782ae85617d5f696ae
dina <dina@byteis.com>
2016-09-24 19:23:44 +0200

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-#mailer action
-config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
-
