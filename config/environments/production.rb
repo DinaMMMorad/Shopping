2563450116587eb7890dd474bfa099d0feecc3b4
dina <dina@byteis.com>
2016-09-25 00:40:01 +0200

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   # config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
-  # Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'https://mallonline.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
