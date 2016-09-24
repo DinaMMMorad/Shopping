767785e903c2e7e470edf1605495cd92f2a3d171
dina <dina@byteis.com>
2016-09-24 19:30:42 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  #mailer
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { :host => 'https://mallonline.herokuapp.com' }
 
 end
