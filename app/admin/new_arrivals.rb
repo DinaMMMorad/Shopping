c6641e5de228e941dee6c9711cc0652cddbc016f
dina <dina@byteis.com>
2016-09-25 22:24:44 +0200

    fixed is new for new arrivals

--- app/admin/new_arrivals.rb
+++ app/admin/new_arrivals.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Product, :as => 'New Arrivals' do
   config.clear_action_items!
   actions :index
 
-  member_action :mark_new_arrival, method: :get do
+  member_action :mark_new_arrival, method: :put do
     if params && params['flag']
       resource.update(:is_new => params['flag'])
     end
@@ -14,7 +14,7 @@ ActiveAdmin.register Product, :as => 'New Arrivals' do
   index do
     column :title
     column :is_new do |p|
-      link_to (check_box_tag 'is_new', :is_new, p.is_new, class: 'is_new'), "new_arrivals/#{p.id}/mark_new_arrival?flag=#{!p.is_new}"
+      link_to (check_box_tag 'is_new', :is_new, p.is_new, class: 'is_new'), mark_new_arrival_admin_new_arrival_path(:id => p.id, :flag => !p.is_new), method: :put
     end
   end
 end
