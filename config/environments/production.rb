2262d9c1f27e5963c2a67b8718f6d617d2a7bb1d
dina <dina@byteis.com>
2016-09-25 14:40:49 +0200

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,7 +121,7 @@ Rails.application.configure do
   # }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mallonline.herokuapp.com'
+  host = 'https://mallonline.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
