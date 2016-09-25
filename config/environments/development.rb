91c9aea2b104bd90fb9169cfb508ba540de6ad7e
dina <dina@byteis.com>
2016-09-25 13:11:36 +0200

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,29 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+
+
+
+
+
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'localhost:3000',
+      authentication: "login",
+      enable_starttls_auto: true,
+      user_name: 'dinammorad1@gmail.com',
+      password: '123456dodo',
+      openssl_verify_mode: 'none'
+  }
+
+
 end
